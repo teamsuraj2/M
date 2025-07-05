1aaa7513ecc7f109275a47ac742222fa138f69a6
Vivekkumar-IN <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-07-05 20:02:11 +0530

    .

--- modules/message.go
+++ modules/message.go
@@ -1,30 +1,25 @@
 package modules
 
-import (
-	"errors"
-	"log"
-)
-
 func OnMessageFnc(m *telegram.NewMessage) error {
-	handlers := []func(*telegram.NewMessage) error{
-		deleteLongMessage,
-		deleteLinkMessage,
-		deleteUserMsgIfBio,
-	}
+    handlers := []func(*telegram.NewMessage) error{
+        deleteLongMessage,
+        deleteLinkMessage,
+        deleteUserMsgIfBio,
+    }
 
-	for _, handler := range handlers {
-		if err := handler(m); err != nil {
-			if errors.Is(err, telegram.EndGroup) {
-				return telegram.EndGroup
-			}
+    for _, handler := range handlers {
+        if err := handler(m); err != nil {
+            if errors.Is(err, telegram.EndGroup) {
+                return telegram.EndGroup
+            }
+          
+            _, e := m.Client.SendMessage(config.LoggerId, "Error in OnMessageFnc: "+err.Error())
+            if e != nil {
+              log.Println("Error in OnMessagFnc error send to grp", err.Error())
+            }
+            return telegram.EndGroup
+        }
+    }
 
-			_, e := m.Client.SendMessage(telegram.LoggerId, "Error in OnMessageFnc: "+err.Error())
-			if e != nil {
-				log.Println("Error in OnMessagFnc error send to grp", err.Error())
-			}
-			return telegram.EndGroup
-		}
-	}
-
-	return nil
+    return nil
 }
