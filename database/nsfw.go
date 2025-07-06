7309b60c85b04542ed41c2c0dd7adb06f69b3754
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-07-06 15:58:09 +0530

    Update nsfw.go

--- database/nsfw.go
+++ database/nsfw.go
@@ -302,19 +302,46 @@ func GetNSFWWords() ([]string, error) {
 }
 
 func SetNSFWFlag(chatID int64, enable bool) error {
+	cacheKey := fmt.Sprintf("%d_nsfw", chatID)
+
+	if cached, ok := config.Cache.Load(cacheKey); ok {
+		if cached.(bool) == enable {
+			return nil
+		}
+	}
+
+	old, err := IsNSFWEnabled(chatID)
+	if err != nil {
+		return err
+	}
+	if old == enable {
+		return nil
+	}
+
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 
-	_, err := nsfwFlagsDB.UpdateOne(
+	_, err = nsfwFlagsDB.UpdateOne(
 		ctx,
 		bson.M{"_id": chatID},
 		bson.M{"$set": bson.M{"enabled": enable}},
 		options.UpdateOne().SetUpsert(true),
 	)
+	if err == nil {
+		config.Cache.Store(cacheKey, enable)
+	}
 	return err
 }
 
 func IsNSFWEnabled(chatID int64) (bool, error) {
+	key := fmt.Sprintf("%d_nsfw", chatID)
+
+	if val, ok := config.Cache.Load(key); ok {
+		if enabled, ok := val.(bool); ok {
+			return enabled, nil
+		}
+	}
+
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 
@@ -325,10 +352,12 @@ func IsNSFWEnabled(chatID int64) (bool, error) {
 	err := nsfwFlagsDB.FindOne(ctx, bson.M{"_id": chatID}).Decode(&result)
 	if err != nil {
 		if err == mongo.ErrNoDocuments {
+			config.Cache.Store(key, false)
 			return false, nil
 		}
 		return false, err
 	}
 
+	config.Cache.Store(key, result.Enabled)
 	return result.Enabled, nil
 }
