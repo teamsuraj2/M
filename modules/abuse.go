cb3d1f68b9b4cf6fdad24b63aba1ca4ae2c2d368
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-07-08 04:12:26 +0530

    Update abuse.go

--- modules/abuse.go
+++ modules/abuse.go
@@ -14,7 +14,7 @@ import (
 
 func init() {
 	AddHelp(
-		"🚫 NoAbuse",
+		"🚫 Abuse",
 		"noabuse_help",
 		"<b>🚫 NoAbuse Filter</b>\n"+
 			"Automatically detects and filters abusive or offensive language in group messages.\n\n"+
