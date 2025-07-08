0e87ccbf604d98984653d66e7653308f37ddcfe1
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-07-08 08:22:12 +0530

    Update dev.go

--- modules/dev.go
+++ modules/dev.go
@@ -186,7 +186,7 @@ func resolveImports(code string) (string, []string) {
 }
 
 func EvalHandle(m *telegram.NewMessage) error {
-	code := strings.TrimSpace(strings.Join(strings.SplitN(m.RawText(true), " ", 2)[1:], " "))
+	code := strings.TrimSpace(strings.Join(strings.SplitN(m.RawText(), " ", 2)[1:], " "))
 	code, imports := resolveImports(code)
 
 	if code == "" {
