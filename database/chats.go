682be9a5565c37a373828277d2ebaacc0ac575ff
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-07-15 13:01:28 +0530

    Update chats.go

--- database/chats.go
+++ database/chats.go
@@ -2,59 +2,55 @@ package database
 
 import (
 	"context"
+	"errors"
 
 	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
 
 	"main/config"
 )
 
-func GetServedChats() ([]int64, error) {
+const chatsIndex = "scChats"
+
+func loadChatCache() ([]int64, error) {
+	if val, ok := config.Cache.Load("chats"); ok {
+		return val.([]int64), nil
+	}
+
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 
-	cursor, err := chatDB.Find(ctx, bson.M{"chat_id": bson.M{"$lt": 0}})
+	var doc struct {
+		ChatIDs []int64 `bson:"chats"`
+	}
+
+	err := chatDB.FindOne(ctx, bson.M{"_id": chatsIndex}).Decode(&doc)
 	if err != nil {
-		return nil, err
-	}
-	defer cursor.Close(ctx)
-
-	var chats []struct {
-		ChatID int64 `bson:"chat_id"`
-	}
-	if err = cursor.All(ctx, &chats); err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return []int64{}, nil
+		}
 		return nil, err
 	}
 
-	var chatIDs []int64
-	for _, chat := range chats {
-		chatIDs = append(chatIDs, chat.ChatID)
-	}
+	config.Cache.Store("chats", doc.ChatIDs)
+	return doc.ChatIDs, nil
+}
 
-	return chatIDs, nil
+func GetServedChats() ([]int64, error) {
+	return loadChatCache()
 }
 
 func IsServedChat(chatID int64) (bool, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), timeout)
-	defer cancel()
-
-	count, err := chatDB.CountDocuments(ctx, bson.M{"chat_id": chatID})
+	chats, err := loadChatCache()
 	if err != nil {
 		return false, err
 	}
-
-	if count > 0 {
-		val, _ := config.Cache.LoadOrStore("chats", []int64{})
-		chats := val.([]int64)
-		for _, id := range chats {
-			if id == chatID {
-				return true, nil
-			}
+	for _, id := range chats {
+		if id == chatID {
+			return true, nil
 		}
-		chats = append(chats, chatID)
-		config.Cache.Store("chats", chats)
-		return true, nil
 	}
-
 	return false, nil
 }
 
@@ -67,7 +63,11 @@ func AddServedChat(chatID int64) error {
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 
-	_, err = chatDB.InsertOne(ctx, bson.M{"chat_id": chatID})
+	_, err = chatDB.UpdateOne(ctx,
+		bson.M{"_id": chatsIndex},
+		bson.M{"$addToSet": bson.M{"chats": chatID}},
+		options.UpdateOne().SetUpsert(true),
+	)
 	if err != nil {
 		return err
 	}
@@ -89,7 +89,10 @@ func DeleteServedChat(chatID int64) error {
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 
-	_, err = chatDB.DeleteOne(ctx, bson.M{"chat_id": chatID})
+	_, err = chatDB.UpdateOne(ctx,
+		bson.M{"_id": chatsIndex},
+		bson.M{"$pull": bson.M{"chats": chatID}},
+	)
 	if err != nil {
 		return err
 	}
@@ -103,6 +106,51 @@ func DeleteServedChat(chatID int64) error {
 			}
 		}
 		config.Cache.Store("chats", chats)
+	}
+
+	return nil
+}
+
+func MigrateChats() error {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	cursor, err := chatDB.Find(ctx, bson.M{"chat_id": bson.M{"$lt": 0}})
+	if err != nil {
+		return err
+	}
+	defer cursor.Close(ctx)
+
+	var oldChats []struct {
+		ChatID int64 `bson:"chat_id"`
+	}
+	if err := cursor.All(ctx, &oldChats); err != nil {
+		return err
+	}
+
+	if len(oldChats) == 0 {
+		return nil
+	}
+
+	chatIDs := make([]int64, 0, len(oldChats))
+	for _, chat := range oldChats {
+		chatIDs = append(chatIDs, chat.ChatID)
+	}
+
+	_, err = chatDB.UpdateOne(ctx,
+		bson.M{"_id": chatsIndex},
+		bson.M{"$addToSet": bson.M{"chats": bson.M{"$each": chatIDs}}},
+		options.UpdateOne().SetUpsert(true),
+	)
+	if err != nil {
+		return err
+	}
+
+	_, err = chatDB.DeleteMany(ctx, bson.M{
+		"chat_id": bson.M{"$in": chatIDs},
+	})
+	if err != nil {
+		return err
 	}
 
 	return nil
