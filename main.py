cfa855bd79abde02434d54cb9a1e941abe36b7e5
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-07-15 18:20:11 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from kivy.app import App
+from kivy.uix.label import Label
+
+class HelloApp(App):
+    def build(self):
+        return Label(text="Hello, Vivek!")
+
+if __name__ == '__main__':
+    HelloApp().run()
